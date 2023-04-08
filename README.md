6d26a961e4026391ba033d4763ae205c02a63545
eTeddy <tpetw886@gmail.com>
2023-04-07 22:27:41 -0500

    createHashRouter

---
+++ README.md
@@ -0,0 +1,28 @@
+# Brian Design
+# React Router
+# createHashRouter
+# react-sidebar-router-v6.4
+
+React Sidebar Navigation Menu using React Router v6.4 - Beginner Tutorial<br>
+https://youtu.be/zQBd3hNXJgI
+
+GitHub Code <br>
+https://github.com/briancodex/react-sidebar-router-v6.4
+
+React Icons <br>
+https://www.npmjs.com/package/react-icons
+
+createHashRouter <br>
+https://reactrouter.com/en/main/routers/create-hash-router
+
+Deploy Vite app to GitHub Pages using GitHub Actions<br>
+https://github.com/sitek94/vite-deploy-demo#deploy-vite-app-to-github-pages-using-github-actions
+
+React Router not working with Github Pages <br>
+https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages
+
+npm create vite@latest react-sidebar-router-v6.4 -- --template react
+
+npm i react-router-dom
+
+npm i react-icons
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "./App.css";
+import React from "react";
+import Products from "./routes/Products";
+import Home from "./routes/Home";
+import Reports from "./routes/Reports";
+import Navbar from "./components/Navbar";
+import { createRoot } from "react-dom/client";
+import {
+  createHashRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const router = createHashRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "reports",
+        element: <Reports />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
